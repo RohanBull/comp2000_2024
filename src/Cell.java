97dcf2e683bee84430dbe9a726683b52e305a18b
Rohan Bull 47417072 <rohan.bull1@students.mq.edu.au>
2024-07-29 16:31:00 +1000

    fixed issues

--- src/Cell.java
+++ src/Cell.java
@@ -7,8 +7,8 @@
     
     public Cell(int x, int y, Graphics g){
         g.setColor(Color.WHITE);
-        g.fillRect(i*35+10, j*35+10, 35, 35);
+        g.fillRect(x*35+10, y*35+10, 35, 35);
         g.setColor(Color.BLACK);
-        g.drawRect(i*35+10, j*35+10, 35, 35);
+        g.drawRect(x*35+10, y*35+10, 35, 35);
         }
  }
--- src/Grid.java
+++ src/Grid.java
@@ -1,11 +1,10 @@
 import java.awt.*;
 public class Grid {
-    Cell[][] cell = neew Cell[20][20];
+    Cell[][] cells = new Cell[20][20];
     public Grid(Graphics g) {
-        for(int i = 0; i < 20; i++){
-            for(int j = 0; j , 20; j++){
+        for (int i = 0; i < 20; i++)
+            for (int j = 0; j < 20; j++) {
                 cells[i][j] = new Cell(i, j, g);
             }
-        }
     }
 }
