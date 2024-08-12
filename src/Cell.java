9a966c61087d6a083825b7fcc3ae2412c372fa0a
Rohan Bull 47417072 <rohan.bull1@students.mq.edu.au>
2024-08-12 14:12:36 +1000

    Finished tast

--- src/Cell.java
+++ src/Cell.java
@@ -1,14 +1,15 @@
  import java.awt.Color;
  import java.awt.Graphics;
+ import java.awt.Point;
 
  public class Cell {
     int x,y;
     int size = 35;
     
-    public Cell(int x, int y, Graphics g){
+    public Cell(int x, int y, Graphics g, Point p){
         g.setColor(Color.WHITE);
-        g.fillRect(x*35+10, y*35+10, 35, 35);
+        g.fillRect(x * size + 10, y * size + 10, size, size);
         g.setColor(Color.BLACK);
-        g.drawRect(x*35+10, y*35+10, 35, 35);
+        g.drawRect(x * size + 10, y * size + 10, size, size);
         }
  }
